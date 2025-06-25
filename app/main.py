0ed203b3a03c59ff2183692259b3ace8bf32427d
rithanyarb <rbrithanya003@gmail.com>
2025-06-25 20:22:11 +0530

    main.py

--- app/main.py
+++ app/main.py
@@ -34,13 +34,7 @@ app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 #CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
-        "https://api-documentation-frontend.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -96,3 +90,4 @@ if __name__ == "__main__":
         reload=True,
         log_level="info"
     )
+
